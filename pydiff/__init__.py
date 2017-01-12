ab4051d706d498005053598ac83b35ce8eebb11e
Gevorg Davoian <davoian.serf@gmail.com>
2017-01-12 19:19:40 +0200

    Add module for conversions of changes

--- pydiff/__init__.py
+++ pydiff/__init__.py
@@ -1,3 +1,5 @@
 from .base import Diff
 from .character import CharacterDiff, diff_characters
 from .word import WordDiff, diff_words
+
+from .convert import convert_changes_to_xml
---
+++ pydiff/convert.py
@@ -0,0 +1,23 @@
+from xml.sax.saxutils import escape as xml_escape
+
+
+def _extended_xml_escape(string):
+    # xml_escape only escapes <, &, and >, so provide an additional mapping for ' and "
+    return xml_escape(string, {'"': '&quot;', "'": "&apos;"})
+
+
+def convert_changes_to_xml(changes):
+    result = []
+    for change in changes:
+        if change.get('added'):
+            result.append('<ins>')
+        elif change.get('removed'):
+            result.append('<del>')
+
+        result.append(_extended_xml_escape(change['value']))
+
+        if change.get('added'):
+            result.append('</ins>')
+        elif change.get('removed'):
+            result.append('</del>')
+    return ''.join(result)
