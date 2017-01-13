20a815072d5abda8cb66accac78f20dd5eac184f
Gevorg Davoian <davoian.serf@gmail.com>
2017-01-13 16:04:15 +0200

    Update setup.py metadata

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='pydiff',
+    author='Gevorg Davoian, Iulius Curt, Kevin Decker (the author of the original jsdiff library) and others',
+    author_email='davoian.serf at gmail.com',
     version='0.0',
-    packages=find_packages()
+    packages=find_packages(),
+    license='Apache 2.0'
 )
