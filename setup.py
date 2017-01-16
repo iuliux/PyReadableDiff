fb99c0f1d1f4ac1bce69c82afbb3549422f06029
Gevorg Davoian <davoian.serf@gmail.com>
2017-01-16 12:40:21 +0200

    Move metadata from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
-from setuptools import setup, find_packages
+import setuptools
 
 
-setup(
-    name='pydiff',
-    description='PyReadableDiff: Intuitive human-readable diff for text',
-    author='Gevorg Davoian, Iulius Curt, Kevin Decker (the author of the original jsdiff library) and others',
-    author_email='davoian.serf at gmail.com',
-    url='https://github.com/BeagleInc/PyReadableDiff',
-    version='0.0',
-    packages=find_packages(),
-    license='Apache-2.0'
-)
+setuptools.setup()
