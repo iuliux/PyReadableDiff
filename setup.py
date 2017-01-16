8d5526665274e56c4c30266e5cc6f4655497db41
Gevorg Davoian <davoian.serf@gmail.com>
2017-01-16 18:16:01 +0200

    Change description in setup.py before uploading to PyPI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import pydiff
 
 setuptools.setup(
     name='PyReadableDiff',
-    description='PyReadableDiff: Intuitive human-readable diff for text',
+    description='Intuitive human-readable diff for text',
     author='Gevorg Davoian, Iulius Curt, Kevin Decker (the author of the original jsdiff library) and others',
     author_email='davoian.serf@gmail.com',
     url='https://github.com/BeagleInc/PyReadableDiff',
