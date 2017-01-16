f7909a4e1d960d82f6ad27956e178ffe959bd5b6
Gevorg Davoian <davoian.serf@gmail.com>
2017-01-16 18:01:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ import pydiff
 
 
 setuptools.setup(
-    name='pydiff',
+    name='PyReadableDiff',
     description='PyReadableDiff: Intuitive human-readable diff for text',
     author='Gevorg Davoian, Iulius Curt, Kevin Decker (the author of the original jsdiff library) and others',
-    author_email='davoian.serf at gmail.com',
+    author_email='davoian.serf@gmail.com',
     url='https://github.com/BeagleInc/PyReadableDiff',
     version=pydiff.__version__,
     packages=setuptools.find_packages(),
-    license='Apache-2.0'
+    license='Apache-2.0',
 )
