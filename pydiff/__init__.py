3e7d82d1efcac9f26a0bf0edc9961bbe83a13955
Gevorg Davoian <davoian.serf@gmail.com>
2017-01-16 13:05:00 +0200

    Add __version__ to pydiff/__init__.py

--- pydiff/__init__.py
+++ pydiff/__init__.py
@@ -10,3 +10,6 @@ from .line import LineDiff, StrippedLineDiff,\
                   diff_lines, diff_stripped_lines
 
 from .convert import convert_changes_to_xml
+
+
+__version__ = '0.0.0'
--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from setuptools import setup, find_packages
+import setuptools
+
+import pydiff
 
 
-setup(
+setuptools.setup(
     name='pydiff',
     description='PyReadableDiff: Intuitive human-readable diff for text',
     author='Gevorg Davoian, Iulius Curt, Kevin Decker (the author of the original jsdiff library) and others',
     author_email='davoian.serf at gmail.com',
     url='https://github.com/BeagleInc/PyReadableDiff',
-    version='0.0',
-    packages=find_packages(),
+    version=pydiff.__version__,
+    packages=setuptools.find_packages(),
     license='Apache-2.0'
 )
