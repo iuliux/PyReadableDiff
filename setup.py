93debee4a04d6b1a2e7b502c2b0172b396c41571
Gevorg Davoian <davoian.serf@gmail.com>
2017-01-17 10:55:43 +0200

    Refactor setup.py and add classifiers

--- setup.py
+++ setup.py
@@ -3,14 +3,51 @@ import setuptools
 import pydiff
 
 
+NAME = 'PyReadableDiff'
+
+DESCRIPTION = 'Intuitive human-readable diff for text'
+
+AUTHOR = 'Gevorg Davoian, Iulius Curt, Kevin Decker ' \
+         '(the author of the original jsdiff library) and others'
+
+AUTHOR_EMAIL = 'davoian.serf@gmail.com'
+
+URL = 'https://github.com/BeagleInc/PyReadableDiff'
+
+VERSION = pydiff.__version__
+
+PACKAGES = setuptools.find_packages()
+
+KEYWORDS = ['python', 'text', 'diff']
+
+CLASSIFIERS = [
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache Software License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Topic :: Software Development :: Libraries :: Python Modules"
+]
+
+LICENSE = 'Apache-2.0'
+
+
 setuptools.setup(
-    name='PyReadableDiff',
-    description='Intuitive human-readable diff for text',
-    author='Gevorg Davoian, Iulius Curt, Kevin Decker (the author of the original jsdiff library) and others',
-    author_email='davoian.serf@gmail.com',
-    url='https://github.com/BeagleInc/PyReadableDiff',
-    version=pydiff.__version__,
-    packages=setuptools.find_packages(),
-    keywords=['python', 'text', 'diff'],
-    license='Apache-2.0'
+    name=NAME,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    version=VERSION,
+    packages=PACKAGES,
+    keywords=KEYWORDS,
+    classifiers=CLASSIFIERS,
+    license=LICENSE
 )
